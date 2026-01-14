a7dd215cf08e8c0ea12effe7b27ffefb89f2b98d
Nirmal Madhunala <154803857+Nirmal-36@users.noreply.github.com>
2026-01-14 17:18:48 +0530

    Add SSL configuration to database settings

--- backend/mrd_system/settings.py
+++ backend/mrd_system/settings.py
@@ -108,6 +108,9 @@ DATABASES = {
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'ssl': {
+                'ca': '/etc/ssl/certs/ca-certificates.crt',
+            },
         },
     }
 }
